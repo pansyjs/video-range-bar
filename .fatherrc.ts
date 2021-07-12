c7907f0097070b8e6fd7acbfbae361b1c400f96f
chenguzhen87 <546369005@qq.com>
2021-07-12 20:09:09 +0800

    chore: dist

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,16 +1,8 @@
-import { readdirSync } from 'fs';
-import { join } from 'path';
 import vue from 'rollup-plugin-vue';
-
-const headPkgs: string[] = ['video-range-bar'];
-const tailPkgs = readdirSync(join(__dirname, 'packages')).filter(
-  (pkg) => pkg.charAt(0) !== '.' && !headPkgs.includes(pkg),
-);
-
 export default {
   esm: 'rollup',
   cjs: 'rollup',
   umd: true,
   extraRollupPlugins: [vue()],
-  pkgs: [...headPkgs, ...tailPkgs],
+  pkgs: ['video-range-bar-react', 'video-range-bar-vue'],
 };
